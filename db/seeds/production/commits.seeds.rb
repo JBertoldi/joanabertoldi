49fe912ebc00591f4c13e48a0c995f69430bfd0b
Joana Bertoldi <joanasoares_1994@msn.com>
2021-02-01 17:07:22 +0000

    Change after development:x to after production:x in production seeds

--- db/seeds/production/commits.seeds.rb
+++ db/seeds/production/commits.seeds.rb
@@ -1,12 +1,12 @@
 require_relative '../get_json'
 require_relative '../create_commits'
 
-after 'development:repos' do
+after 'production:repos' do
   repos = Joana.last.repos
 
   repos.each do |repo|
     url = repo.commits_url
-    json = get_json(url)
+    json = get_json(url, production: true)
 
     puts 'Creating commits..'
     create_commits(json, repo.id)
