9156c2f800882f1286269dd1c75f8fb0b93e7e01
Joana Bertoldi <joanasoares_1994@msn.com>
2021-02-08 17:47:41 +0000

    Change create_or_find_by to find_or_create_by in create commits and technologies

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,4 +2,6 @@ class Commit < ApplicationRecord
   belongs_to :repo, counter_cache: true
 
   validates :html_url, format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/ }
+
+  scope :time_ordered, -> { order(gh_created_at: :desc) }
 end
--- db/seeds/create_commits.rb
+++ db/seeds/create_commits.rb
@@ -8,7 +8,7 @@ def create_commits(json, repo_id)
   json.each do |commit|
     next if commit[:author][:login] != Joana.last.username
 
-    Commit.create_or_find_by(
+    Commit.find_or_create_by(
       message: commit[:commit][:message],
       html_url: commit[:html_url],
       gh_created_at: commit[:commit][:author][:date],
